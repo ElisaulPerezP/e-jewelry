7a7c0a9af0a7472566a9938d8457d975d6f9eaba
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-03-31 23:19:40 -0500

    feat: create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@jewelry.com',
+
+        ]);
+
+        User::factory()
+            ->count(1000)
+            ->create();
+    }
+}

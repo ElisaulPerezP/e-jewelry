42e56a4bde98d884faccb77c0cff17d8baf8f3d0
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-04-02 14:02:00 -0500

    style: php-cs-fixer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -19,7 +18,6 @@ return new class() extends Migration {
             $table->timestamps();
         });
     }
-
 
     public function down(): void
     {

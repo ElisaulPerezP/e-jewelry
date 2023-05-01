4a4f2391d4b752c7ec05c18b9f1ebc867b910650
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-04-30 23:14:32 -0500

    feat: add translation for categories and subcategories

--- lang/es/boxes.php
+++ lang/es/boxes.php
@@ -19,5 +19,6 @@ return [
     'userDetail' => 'Detalle de usuario',
     'seeUsers' => 'Ver usuarios',
     'password' => 'Contraseña',
+    'navigationUsers' => 'Navegador de usuarios',
     'restZone' => 'En esta area es afectado el registro, por favor confirme su responsabilidad ingresando su contraseña.',
 ];

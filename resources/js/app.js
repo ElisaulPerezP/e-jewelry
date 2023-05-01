c6f85d8911342ad8017b0695a6e95bc2974e6727
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-05-01 00:41:00 -0500

    feat: initialize Vue application with AlpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import './bootstrap';
-
+import {createApp} from 'vue'
 import Alpine from 'alpinejs';
-
+import App from './App.vue'
 window.Alpine = Alpine;
 
 Alpine.start();
+createApp(App).mount("#app")

48973886e80c838ce2311121bba0441e82b31a0b
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-05-05 10:49:00 -0500

    feat: api authentication by passport

--- routes/api.php
+++ routes/api.php
@@ -4,30 +4,11 @@ use App\Http\Controllers\Product\ApiProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/tokens/create', function () {
-    if (auth()->user()) {
-        $token = auth()->user()->createToken('apiToken');
-        return ['token' => $token->plainTextToken];
-    }
-    return ['token' => null];
-});
-
-Route::prefix('api')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/products', [ApiProductController::class, 'index'])->name('api.products.index');
     Route::get('/products/{product}', [ApiProductController::class, 'show'])->name('api.products.show');
     Route::post('/products', [ApiProductController::class, 'store'])->name('api.products.store');

ed7a6cdcf794f63675662957c31095c433540a6e
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-05-09 19:09:52 -0500

    fix: bar code rule

--- resources/js/components/products/ProductsIndex.vue
+++ resources/js/components/products/ProductsIndex.vue
@@ -5,22 +5,22 @@
                 <section class="container mx-auto p-6 font-mono">
                     <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
                         <button @click="newProduct"
-                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xl text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             Nuevo producto
                         </button>
                         <div class="w-full overflow-x-auto">
-                            <table class="w-full text-center border-separate">
-                                <thead>
+                            <table class="w-full text-xs border-separate">
+                                <thead class="text-sm">
                                 <tr
                                     class="text-md font-semibold tracking-wide text-left text-gray-900 bg-blue-100 uppercase border-b border-gray-600">
-                                    <th class="px-4 py-3">{{ ('name') }}</th>
-                                    <th class="px-4 py-3">{{ ('description') }}</th>
-                                    <th class="px-4 py-3">{{ ('price') }}</th>
+                                    <th class="px-4 py-3">{{ ('nombre') }}</th>
+                                    <th class="px-4 py-3">{{ ('descripción') }}</th>
+                                    <th class="px-4 py-3">{{ ('precio') }}</th>
                                     <th class="px-4 py-3">{{ ('stock') }}</th>
-                                    <th class="px-4 py-3">{{ ('score') }}</th>
-                                    <th class="px-4 py-3">{{ ('enabling') }}</th>
-                                    <th class="px-4 py-3">{{ ('barCode') }}</th>
-                                    <th class="px-4 py-3">{{ ('image') }}</th>
+                                    <th class="px-4 py-3">{{ ('puntos') }}</th>
+                                    <th class="px-4 py-3">{{ ('estado') }}</th>
+                                    <th class="px-4 py-3">{{ ('codigo') }}</th>
+                                    <th class="px-4 py-3">{{ ('imagen') }}</th>
                                     <th class="px-4 py-3">{{ ('ver') }}</th>
                                     <th class="px-4 py-3">{{ ('editar') }}</th>
                                 </tr>
@@ -76,7 +76,7 @@
                                     <td class="px-4 py-3 border">
                                         <div class="flex items-center text-sm">
                                             <a :href="'/products/show/' + product.id"
-                                               class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                               class="inline-flex items-center px-1 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                                 <slot>Detalle</slot>
                                             </a>
                                         </div>
@@ -84,7 +84,7 @@
                                     <td class="px-4 py-3 border">
                                         <div class="flex items-center text-sm">
                                             <a :href="'/products/edit/' + product.id"
-                                               class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                               class="inline-flex items-center px-1 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                                 <slot>Editar</slot>
                                             </a>
                                         </div>
@@ -94,7 +94,7 @@
                             </table>
                         </div>
                         <button @click="back"
-                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                class="inline-flex items-center px-1 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             Atras
                         </button>
                     </div>

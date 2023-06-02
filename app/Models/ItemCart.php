833991adccc1eebb7cfc6d4c1181d2188e6b5ff1
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-01 20:28:04 -0500

    feat: Implement ItemCart model with User and Product relationships

---
+++ app/Models/ItemCart.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ItemCart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount',
+        'item_state',
+    ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}

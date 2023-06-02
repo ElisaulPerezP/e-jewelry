4d7a8f347f61daa1eb4171da0ac95d32d3dd99dc
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-01 20:41:45 -0500

    feat: add ItemCartRequest for data validation

---
+++ app/Http/Resources/ItemCartResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ItemCartResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'itemCart_id' => $this->id,
+            'user_id' => $this->user_id,
+            'product_id' => $this->product_id,
+            'amount' => $this->amount,
+            'item_state' => $this->item_state,
+            'product_image' => $this->product->image,
+            'product_name' => $this->product->name,
+            'products_price' => $this->product->price,
+        ];
+    }
+}

935a78f8b900a9b5e33cf3e412d74593562c75cd
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-01 22:57:16 -0500

    feat: Cart button on dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <div class="flex justify-between">
+        <div class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ trans('general.dashboard') }}
-        </h2>
+        </div>
+        <div class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <a href="{{ route('cart', Auth::user()) }}" class="ml-4 font-semi-bold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">CARRITO</a>
+        </div>
+        </div>
     </x-slot>
 
     <div class="py-12">

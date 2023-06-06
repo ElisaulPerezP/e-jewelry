3fbb2b69658f7988d6e298acfeb5f989d72e60fe
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-06 18:10:53 -0500

    feat(front-end): Add checkbox for order assembly and total amount calculation

--- resources/js/components/cart/Cart.vue
+++ resources/js/components/cart/Cart.vue
@@ -127,7 +127,9 @@
                                             <div>
                                                 <button @click="newProduct"
                                                         class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xl text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                                    {{ totalPrice === 0 ? 'NO HA SELECCIONADO PRODUCTOS' : 'PAGAR AHORA' }}
+                                                    {{
+                                                        totalPrice === 0 ? 'NO HA SELECCIONADO PRODUCTOS' : 'PAGAR AHORA'
+                                                    }}
                                                 </button>
                                             </div>
 

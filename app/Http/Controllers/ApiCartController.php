53025f0750ee2347186e547f54c056288e80681a
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-06 17:52:23 -0500

    feat: Add controllers for state and amount

--- app/Http/Controllers/ApiCartController.php
+++ app/Http/Controllers/ApiCartController.php
@@ -16,6 +16,7 @@ class ApiCartController extends Controller
 {
     public function index(User $user): AnonymousResourceCollection
     {
+        Cache::forget('cart');
         $itemCarts = Cache::rememberForever('cart', function () use ($user) {
             return $user->itemCarts()->get();
         });
@@ -44,6 +45,80 @@ class ApiCartController extends Controller
         return new ItemCartResource($itemCart);
     }
 
+    public function updateItemStateToSaved(ItemCart $itemCart): ItemCartResource|string
+    {
+        $itemCart->item_state = 'saved';
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
+
+
+
+    public function updateItemStateToInCart(ItemCart $itemCart): ItemCartResource|string
+    {
+        $itemCart->item_state = 'in_cart';
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
+    public function updateItemStateToInOrder(ItemCart $itemCart): ItemCartResource|string
+    {
+        $itemCart->item_state = 'in_order';
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
+    public function updateExpireDate(ItemCart $itemCart): ItemCartResource|string
+    {
+        $now = time();
+        $expireUnixDate = $now + 3600 * 2;
+        $itemCart->expire_date = $expireUnixDate;
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
+
+    public function resetItemAmount(ItemCart $itemCart): ItemCartResource|JsonResponse
+    {
+        $product = Product::find($itemCart->product->id);
+        $product->stock += $itemCart->amount;
+        $product->save();
+
+        $itemCart->amount = 0;
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
+    public function updateAmount(ItemCartRequest $request, ItemCart $itemCart): ItemCartResource|JsonResponse
+    {
+        if ($itemCart->product()->first()->stock < $request->amount - $itemCart->amount) {
+            return response()->json(['error' => 'Lo sentimos, hay '
+                . $itemCart->product()->first()->stock + $itemCart->amount
+                . ' disponibles'], 422);
+        }
+
+        $product = Product::find($itemCart->product->id);
+        $product->stock -= ($request->amount - $itemCart->amount);
+        $product->save();
+
+        $itemCart->amount = $request->amount;
+        $itemCart->save();
+
+        Cache::forget('cart');
+
+        return new ItemCartResource($itemCart);
+    }
     public function show(Product $product): ProductResource
     {
         return new ProductResource($product);
@@ -51,19 +126,21 @@ class ApiCartController extends Controller
 
     public function store(Product $product): ItemCartResource|string
     {
-        $itemCart = ItemCart::where('product_id', $product->id)->first();
+        $itemCart = ItemCart::where('product_id', $product->id)->where('user_id', auth()->user()->id)->first();
 
         if (is_null($itemCart)) {
+            $now = time();
+            $expireUnixDate = $now + 3600 * 2;
             $itemCart = new ItemCart();
-            $itemCart->user_id = auth()->id();
+            $itemCart->user_id = auth()->user()->id;
             $itemCart->product_id = $product->id;
             $itemCart->amount = 1;
             $itemCart->item_state = 'in_cart';
+            $itemCart->expire_date = $expireUnixDate;
 
             $itemCart->save();
+            Cache::forget('cart');
         }
-
-        Cache::forget('cart');
 
         return new ItemCartResource($itemCart);
     }

80cd70bcb6b9ebeb3db6f62532fbf13abebbbc4a
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-10 08:17:12 -0500

    feat: Adapt Order model for payment gateway integration

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,24 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'id_user',
-        'id_product',
-        'amount',
-        'item_state',
+        'payment_reference',
+        'description',
+        'total',
+        'currency',
+        'order_state',
+        'expiration',
+        'return_url',
+        'process_url',
+        'request_id',
     ];
 
-    public function user()
+    public function completed(): void
     {
-        return $this->belongsTo(User::class, 'usuario_id');
+        $this->order_state = 'approved';
+        $this->save();
     }
-    public function product()
+    public function canceled(): void
     {
-        return $this->belongsTo(Product::class, 'producto_id');
+        $this->order_state = 'reject';
+        $this->save();
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function itemCarts(): HasMany
+    {
+        return $this->hasMany(ItemCart::class, 'itemCart_id');
     }
 }

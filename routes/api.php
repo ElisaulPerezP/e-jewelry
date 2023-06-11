72ad7a0fd7b654afaed35cbdbc046307559e3e3a
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-06-11 12:51:08 -0500

    refactor: Restructure api route file for improved organization

--- routes/api.php
+++ routes/api.php
@@ -3,33 +3,43 @@
 use App\Http\Controllers\ApiCartController;
 use App\Http\Controllers\ApiOrderController;
 use App\Http\Controllers\ApiProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/products', [ApiProductController::class, 'index'])->name('api.products.index');
 
-Route::get('/cart/{user}', [ApiCartController::class, 'index'])->name('api.cart.index')->middleware('auth:api');
-Route::put('/cart/{itemCart}/update/amount', [ApiCartController::class, 'updateAmount'])->name('api.cart.update.amount')->middleware('auth:api');
-Route::post('/cart/{product}/store', [ApiCartController::class, 'store'])->name('api.cart.store')->middleware('auth:api', 'auth');
-Route::delete('/cart/{itemCart}/delete', [ApiCartController::class, 'destroy'])->name('api.cart.destroy')->middleware('auth:api');
-Route::put('/cart/{itemCart}/update/date', [ApiCartController::class, 'updateExpireDate'])->name('api.cart.update.date')->middleware('auth:api');
-Route::put('/cart/{itemCart}/update/state/saved', [ApiCartController::class, 'updateItemStateToSaved'])->name('api.cart.update.state.saved')->middleware('auth:api');
-Route::put('/cart/{itemCart}/update/state/incart', [ApiCartController::class, 'updateItemStateToInCart'])->name('api.cart.update.state.incart')->middleware('auth:api');
-Route::put('/cart/{itemCart}/reset/amount', [ApiCartController::class, 'resetItemAmount'])->name('api.cart.reset.item.amount')->middleware('auth:api');
+Route::middleware('auth:api')->name('api.')->group(function () {
+    Route::prefix('/cart')->name('cart.')->group(function () {
+        Route::get('/', [ApiCartController::class, 'index'])
+            ->name('index');
+        Route::put('/{itemCart}/setAmount', [ApiCartController::class, 'setAmount'])
+            ->name('setAmount');
+        Route::post('/{product}/store', [ApiCartController::class, 'store'])
+            ->name('store')->middleware('auth');
+        Route::delete('/{itemCart}/delete', [ApiCartController::class, 'destroy'])
+            ->name('destroy');
+        Route::put('/{itemCart}/changeState', [ApiCartController::class, 'changeState'])
+            ->name('update.state.saved');
+    });
 
-Route::get('/order/', [ApiOrderController::class, 'index'])->name('api.order.index')->middleware('auth:api');
-Route::post('/order/create/', [ApiOrderController::class, 'store'])->name('api.order.store')->middleware('auth:api');
-Route::get('/order/{order}/checkStatus', [ApiOrderController::class, 'checkStatus'])->name('api.order.show')->middleware('auth:api');
+    Route::prefix('/order')->name('order.')->group(function () {
+        Route::get('/', [ApiOrderController::class, 'index'])
+            ->name('index');
+        Route::post('/create', [ApiOrderController::class, 'store'])
+            ->name('store');
+        Route::get('/{order}/checkStatus', [ApiOrderController::class, 'checkStatus'])
+            ->name('show');
+    });
 
-Route::middleware(['auth:api', 'role:admin'])->group(function () {
-    Route::get('/products/{product}', [ApiProductController::class, 'show'])->name('api.products.show');
-    Route::post('/products', [ApiProductController::class, 'store'])->name('api.products.store');
-    Route::put('/products/{product}', [ApiProductController::class, 'update'])->name('api.products.update');
-    Route::delete('/products/{product}', [ApiProductController::class, 'destroy'])->name('api.products.destroy');
-    Route::put('/products/changeStatus/{product}', [ApiProductController::class, 'changeStatus'])
-        ->name('api.products.changeStatus');
+    Route::middleware('role:admin')->prefix('/products')->name('products.')->group(function () {
+        Route::get('/{product}', [ApiProductController::class, 'show'])
+            ->name('show');
+        Route::post('/', [ApiProductController::class, 'store'])
+            ->name('store');
+        Route::put('/{product}', [ApiProductController::class, 'update'])
+            ->name('update');
+        Route::delete('/{product}', [ApiProductController::class, 'destroy'])
+            ->name('destroy');
+        Route::put('/{product}/changeStatus', [ApiProductController::class, 'changeStatus'])
+            ->name('changeStatus');
+    });
 });

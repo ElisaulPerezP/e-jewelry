46786a85127b46059227447051595871703d99b0
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-07-10 12:15:23 -0500

    feat: Implement resource actions for role creation and deletion

---
+++ app/Actions/Permissions/CreateRoleAction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Actions\Permissions;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Spatie\Permission\Models\Role;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class CreateRoleAction
+{
+    public function execute(Request $request): JsonResponse
+    {
+        Cache::forget('roles');
+        $name = $request->input('name');
+        $guardApi = $request->input('guardApi');
+        $guardWeb = $request->input('guardWeb');
+
+        $createdRoles = [];
+
+        if ($guardApi === 'true') {
+            $roleApi = Role::findOrCreate($name, 'api');
+            $createdRoles[] = $roleApi;
+        }
+        if ($guardWeb === 'true') {
+            $roleWeb = Role::findOrCreate($name, 'web');
+            $createdRoles[] = $roleWeb;
+        }
+
+        if (count($createdRoles) > 0) {
+            return response()->json([
+                'message' => 'Roles created successfully',
+                'roles' => $createdRoles,
+            ], ResponseAlias::HTTP_CREATED);
+        } else {
+            return response()->json([
+                'message' => 'No roles were created',
+            ], ResponseAlias::HTTP_BAD_REQUEST);
+        }
+    }
+}

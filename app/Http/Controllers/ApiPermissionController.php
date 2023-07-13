7f57c11b1c3f4ff2c8a9a2bd5c9c1879fbe60614
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-07-12 21:44:56 -0500

    feat: Add API controller for role assignment

--- app/Http/Controllers/ApiPermissionController.php
+++ app/Http/Controllers/ApiPermissionController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Actions\Permissions\AssignPermissionsToRoleAction;
 use App\Actions\Permissions\AssignPermissionsToUserAction;
+use App\Actions\Permissions\AssignRolesToUserAction;
 use App\Actions\Permissions\GetPaginatedPermissionsAction;
+use App\Actions\Permissions\GetPaginatedRolesToUserAction;
 use App\Http\Requests\IndexRequest;
 use App\Http\Requests\PermissionsRequest;
+use App\Http\Requests\Users\RolesRequest;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Spatie\Permission\Models\Role;
@@ -26,5 +29,14 @@ class ApiPermissionController extends Controller
     public function assignPermissionsToUser(PermissionsRequest $request, User $user): AnonymousResourceCollection
     {
         return (new AssignPermissionsToUserAction())->execute($request, $user);
+    }
+    public function showRolesToUser(indexRequest $request, User $user): AnonymousResourceCollection
+    {
+        return (new GetPaginatedRolesToUserAction())->execute($request, $user);
+    }
+
+    public function assignRolesToUser(RolesRequest $request, User $user): AnonymousResourceCollection
+    {
+        return (new AssignRolesToUserAction())->execute($request, $user);
     }
 }

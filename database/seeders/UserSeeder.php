6505c93ada88e05ff9bc4c9c606cc5b55c201cd9
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-07-14 22:47:10 -0500

    feat: Declare permissions and roles, assign permissions to roles, and assign roles to users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,13 +15,20 @@ class UserSeeder extends Seeder
             'email' => 'admin@jewelry.com',
         ]);
 
-        $admin->assignRole('admin');
+        $roleApiAdmin = Role::findOrCreate('admin', 'api');
+        $roleWebAdmin = Role::findOrCreate('admin', 'web');
+
+        $admin->assignRole([$roleApiAdmin, $roleWebAdmin]);
 
         User::factory()
             ->count(10)
             ->create();
-        User::all()->each(function ($user) {
-            $user->assignRole('user');
+        $roleApiUser = Role::findOrCreate('user', 'api');
+        $roleWebUser = Role::findOrCreate('user', 'web');
+
+        User::all()->each(function ($user) use ($roleApiUser, $roleWebUser) {
+            $user->assignRole($roleApiUser);
+            $user->assignRole($roleWebUser);
         });
     }
 }

45bae797121a9863aa4ed86defd2bc2cdd4da689
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-07-14 22:41:33 -0500

    feat: Restructure routes file to leverage the permission system

--- routes/api.php
+++ routes/api.php
@@ -14,29 +14,40 @@ Route::get('/products', [ApiProductController::class, 'index'])->name('api.produ
 Route::middleware('auth:api')->name('api.')->group(function () {
     Route::prefix('/cart')->name('cart.')->group(function () {
         Route::get('/', [ApiCartController::class, 'index'])
+            ->middleware('role_or_permission:api.index.cart|admin')
             ->name('index');
         Route::put('/{cartItem}/setAmount', [ApiCartController::class, 'setAmount'])
+            ->middleware('role_or_permission:api.setAmount.cart|admin')
             ->name('setAmount');
         Route::post('/{product}/store', [ApiCartController::class, 'store'])
-            ->name('store')->middleware('auth');
+            ->middleware('role_or_permission:api.store.cart|admin')
+            ->name('store');
         Route::delete('/{cartItem}/delete', [ApiCartController::class, 'destroy'])
+            ->middleware('role_or_permission:api.destroy.cart|admin')
             ->name('destroy');
         Route::put('/{cartItem}/changeState', [ApiCartController::class, 'changeState'])
+            ->middleware('role_or_permission:api.changeState.cart|admin')
             ->name('update.state.saved');
         Route::get('/total', [ApiCartController::class, 'total'])
+            ->middleware('role_or_permission:api.total.cart|admin')
             ->name('total');
     });
 
     Route::prefix('/order')->name('order.')->group(function () {
         Route::get('/', [ApiOrderController::class, 'index'])
+            ->middleware('role_or_permission:api.order.index|admin')
             ->name('index');
         Route::get('/{order}/show', [ApiOrderController::class, 'show'])
+            ->middleware('role_or_permission:api.order.show|admin')
             ->name('show');
         Route::post('/create', [ApiOrderController::class, 'store'])
+            ->middleware('role_or_permission:api.order.store|admin')
             ->name('store');
         Route::get('/{order}/checkStatus', [ApiOrderController::class, 'checkStatus'])
+            ->middleware('role_or_permission:api.order.store|admin')
             ->name('checkStatus');
         Route::post('/{order}/retry', [ApiOrderController::class, 'retry'])
+            ->middleware('role_or_permission:api.order.retry|admin')
             ->name('retry');
     });
 
@@ -63,43 +74,56 @@ Route::middleware('auth:api')->name('api.')->group(function () {
     Route::get('/export/products', [ApiDTOController::class, 'export'])
         ->middleware('role_or_permission:api.export.product|admin')
         ->name('api.export.products');
+
     Route::post('/import/products', [ImportController::class, 'store'])
         ->middleware('role_or_permission:api.import.product|admin')
         ->name('api.import.products');
 
     Route::middleware('role:admin')->prefix('/permissions')->name('permissions.')->group(function () {
         Route::get('/', [ApiPermissionController::class, 'index'])
+            ->middleware('role_or_permission:api.permissions.index|admin')
             ->name('index');
     });
 
     Route::middleware('role:admin')->prefix('/roles')->name('roles.')->group(function () {
         Route::delete('/{role}', [ApiRolesController::class, 'delete'])
+            ->middleware('role_or_permission:api.roles.delete|admin')
             ->name('delete');
         Route::get('/{role}/permissions', [ApiRolesController::class, 'showPermissionsToRole'])
+            ->middleware('role_or_permission:api.permissionsToRole.show|admin')
             ->name('permissions');
         Route::post('/store', [ApiRolesController::class, 'store'])
+            ->middleware('role_or_permission:api.roles.store|admin')
             ->name('store');
         Route::get('/', [ApiRolesController::class, 'index'])
+            ->middleware('role_or_permission:api.roles.index|admin')
             ->name('index');
         Route::post('/{role}/permissions', [ApiPermissionController::class, 'assignPermissionsToRole'])
+            ->middleware('role_or_permission:api.roles.assignPermissions|admin')
             ->name('permissions.store');
     });
 
     Route::middleware('role:admin')->prefix('/users')->name('users.')->group(function () {
         Route::get('/{user}/permissions', [ApiRolesController::class, 'showPermissionsToUser'])
+            ->middleware('role_or_permission:api.user.showPermissions|admin')
             ->name('permissions');
         Route::post('/{user}/permissions', [ApiPermissionController::class, 'assignPermissionsToUser'])
+            ->middleware('role_or_permission:api.user.assignPermissions|admin')
             ->name('permissions.store');
         Route::get('/{user}/roles', [ApiPermissionController::class, 'showRolesToUser'])
-            ->name('roles.store');
+            ->middleware('role_or_permission:api.user.showRoles|admin')
+            ->name('roles.show');
         Route::post('/{user}/roles', [ApiPermissionController::class, 'assignRolesToUser'])
+            ->middleware('role_or_permission:api.user.assignRoles|admin')
             ->name('roles.store');
     });
 
     Route::middleware('role:admin')->prefix('/identity')->name('identity.')->group(function () {
         Route::get('/role/{role}', [ApiRolesController::class, 'roleIdentity'])
+            ->middleware('role_or_permission:api.role.identity|admin')
             ->name('role');
         Route::get('/user/{user}', [ApiRolesController::class, 'userIdentity'])
+            ->middleware('role_or_permission:api.user.identity|admin')
             ->name('user');
     });
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartItems\CartController;
+use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\Orders\OrderController;
 use App\Http\Controllers\PermissionsController;
 use App\Http\Controllers\Products\ProductController;
@@ -15,32 +16,57 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
-Route::get('/cart/{user}', [CartController::class, 'index'])->middleware(['auth', 'verified'])->name('cart');
+Route::get('/cart/{user}', [CartController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('cart');
+Route::get('/order', [OrderController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('orders');
+Route::get('/order/{order}/show', [OrderController::class, 'show'])
+    ->middleware(['auth', 'verified'])
+    ->name('orders.show');
 
-Route::get('/order', [OrderController::class, 'index'])->middleware(['auth', 'verified'])->name('orders');
-Route::get('/order/{order}/show', [OrderController::class, 'show'])->middleware(['auth', 'verified'])->name('orders.show');
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{user}', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::put('/users/changeStatus/{user}', [UserController::class, 'changeStatus'])->name('users.changeStatus');
-    Route::get('/users/show/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::put('/users/{user}/permissions', [UserController::class, 'assignPermissionsToUser'])->name('users.assignPermissions');
-    Route::put('/users/{user}/roles', [UserController::class, 'assignRolesToUser'])->name('users.assignRoles');
+    Route::get('/users', [UserController::class, 'index'])
+        ->name('users.index');
+    Route::get('/users/{user}', [UserController::class, 'edit'])
+        ->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])
+        ->name('users.update');
+    Route::put('/users/changeStatus/{user}', [UserController::class, 'changeStatus'])
+        ->name('users.changeStatus');
+    Route::get('/users/show/{user}', [UserController::class, 'show'])
+        ->name('users.show');
+    Route::put('/users/{user}/permissions', [UserController::class, 'assignPermissionsToUser'])
+        ->name('users.assignPermissions');
+    Route::put('/users/{user}/roles', [UserController::class, 'assignRolesToUser'])
+        ->name('users.assignRoles');
 
     Route::resource('/products', ProductController::class);
-    Route::get('/permissions/index', [PermissionsController::class, 'index'])->name('permissions.index');
-    Route::get('/roles/index', [RolesController::class, 'index'])->name('roles.index');
-    Route::get('/roles/{role}', [RolesController::class, 'assignPermissionsToRole'])->name('roles.permissions');
+    Route::get('/permissions/index', [PermissionsController::class, 'index'])
+        ->name('permissions.index');
+    Route::get('/roles/index', [RolesController::class, 'index'])
+        ->name('roles.index');
+    Route::get('/roles/{role}', [RolesController::class, 'assignPermissionsToRole'])
+        ->name('roles.permissions');
 });
+
+Route::get('/dev/showAuthorizationCode/', [ClientsController::class, 'showCode'])
+    ->middleware('role_or_permission:user.dev|admin')
+    ->name('client.code.show');
+Route::get('/dev/{user}', [ClientsController::class, 'index'])
+    ->middleware('role_or_permission:user.dev|admin')
+    ->name('client.index');
 
 require __DIR__ . '/auth.php';
